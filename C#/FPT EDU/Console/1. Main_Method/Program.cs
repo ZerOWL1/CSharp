12dbf3e20ac6f7d051fb3c0c4789b56ae3d2cd39
Lục Dương Hải <haild.works@gmail.com>
2021-05-18 16:11:36 +0700

    Add files via upload

---
+++ C#/FPT EDU/Console/1. Main_Method/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.Main_Method
+{
+    class Program
+    {
+        //Cach 1: Khong co kieu tra ve, co tham so la mang 'string'
+        #region main_Method with string[] args
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World!!");
+
+            Console.ReadLine();
+        }*/
+        #endregion
+
+        //Cach 2: Khong co kieu tra ve, khong co tham so 
+
+        #region main_Method no return and string[] args
+        /*static void Main()
+        {
+            Console.WriteLine($"Hello World!!");
+
+            Console.ReadLine();
+        }
+        */
+        #endregion
+
+        //Cach 3: Co kieu tra ve, khong tham so
+        #region main_Method have return
+        /*static int Main()
+        {
+            Console.WriteLine($"Hello World!!");
+
+            Console.ReadLine();
+            return 0;
+        }*/
+        #endregion
+
+        //Cach 4: Co kieu tra ve, co tham so
+        #region main_Method have return and string[] args
+        /*static int Main(string[] args)
+        {
+            Console.WriteLine($"Hello World!!");
+
+            Console.ReadLine();
+            return 0;
+        }*/
+        #endregion
+
+
+
+        
+
+    }
+}
