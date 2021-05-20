8c5a86608bca6794ed107ff825c15665b0e88646
Lục Dương Hải <haild.works@gmail.com>
2021-05-20 15:25:22 +0700

    Add files via upload

---
+++ C#/FPT EDU/LAB C#/None-Validate/Labs/Lab1_HE141223_HaiLD/Controllers/CalculatorControl.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1_HE141223_HaiLD.Controllers
+{
+    class CalculatorControl
+    {
+        //declare array with 1000 value
+        private static int[] arr = new int[1000];
+
+        //declare count to save value range in array
+        private static int count = 0;
+
+
+        //search add method
+        public static void addValue()
+        {
+            //check if array[count] > 1000
+            if (count > 1000)
+            {
+                Console.WriteLine("Array Full 1000 values!!");
+            }
+            else
+            {
+                //add value 
+                int value = int.Parse(Console.ReadLine());
+
+                //set value at count index and count += 1 also print message
+                arr.SetValue(value, count);
+                count++;
+                Console.WriteLine();
+            }
+        }
+
+        //search value method
+        public static void showValue()
+        {
+            //check if array[count] == 0 <=> array no value
+            if (count == 0)
+            {
+                Console.WriteLine("There're no Value!!");
+            }
+            else
+            {
+                //show value with i < count
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write($"{arr[i]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        //search Value method
+        public static void searchValue()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            //check value in array exist or not
+            if (arr.Contains(input))
+            {
+                //set countIndex support show value
+                int countIndex = 0;
+
+                //show value at index
+                for (int i = 0; i < count; i++)
+                {
+                    if (arr[i] == input)
+                    {
+                        Console.WriteLine($"Value: {arr[i]} at index {i}");
+                        countIndex++;
+                    }
+                }
+                Console.WriteLine($"Found {countIndex} value {input} in Array!");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+                Console.WriteLine();
+            }
+        }
+
+        //show details
+        public static void showStatistics()
+        {
+            //int sum = arr.Sum();
+
+            float average = (float)Sum() / count;
+
+
+            Console.WriteLine($"Total data: {count}");
+            Console.WriteLine($"Sum: {Sum()}");
+            Console.WriteLine($"Average: {average}");
+            findMinMax(arr);
+            Console.WriteLine();
+        }
+
+        //find min, max func
+        public static void findMinMax(int[] numbers)
+        {
+            int min = numbers[0];
+            int max = numbers[0];
+
+            for (int i = 0; i < count; i++)
+            {
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+                else if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+            Console.WriteLine($"Min: {min}");
+            Console.WriteLine($"Max: {max}");
+        }
+
+        //find sum
+        public static int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ C#/FPT EDU/LAB C#/None-Validate/Labs/Lab1_HE141223_HaiLD/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lab1_HE141223_HaiLD.Controllers;
+
+namespace Lab1_HE141223_HaiLD
+{
+    class Program
+    {
+        /// <summary>
+        /// Lab 1.
+        ///  - Luc Duong Hai - HaiLD _ HE141223
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        Enter:
+            Console.WriteLine("1. Add. ");
+            Console.WriteLine("2. Show. ");
+            Console.WriteLine("3. Search. ");
+            Console.WriteLine("4. Statistics. ");
+            Console.WriteLine("5. Exit. ");
+            Console.Write("Enter a option (1-5): ");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                default:
+                    Console.WriteLine("| Not found option!!");
+                    goto Enter;
+                case 1:
+                    {
+                        CalculatorControl.addValue();
+                        goto Enter;
+                    }
+                case 2:
+                    {
+                        CalculatorControl.showValue();
+                        goto Enter;
+                    }
+                case 3:
+                    {
+                        CalculatorControl.searchValue();
+                        goto Enter;
+                    }
+                case 4:
+                    {
+                        CalculatorControl.showStatistics();
+                        goto Enter;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("You've want to EXIT!! Thanks for using!");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
