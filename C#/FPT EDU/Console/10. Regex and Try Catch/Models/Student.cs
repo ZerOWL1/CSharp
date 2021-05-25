5a7c2d77a30320594106a28ef3604165e495cbaa
Lục Dương Hải <haild.works@gmail.com>
2021-05-26 02:42:34 +0700

    Update Regex Code

---
+++ C#/FPT EDU/Console/10. Regex and Try Catch/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10.Regex_and_Try_Catch.Models
+{
+    class Student
+    {
+
+        private string Roll;
+        private string Name;
+        private string Email;
+
+
+        public Student()
+        {
+        }
+
+        public void InputInfo(string roll, string name, string email)
+        {
+            Roll1 = roll;
+            Name1 = name;
+            Email1 = email;
+        }
+
+
+        public string Roll1 { get => Roll; set => Roll = value; }
+        public string Name1 { get => Name; set => Name = value; }
+        public string Email1 { get => Email; set => Email = value; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"| Roll: {this.Roll1}");
+            Console.WriteLine($"| Name: {this.Name1}");
+            Console.WriteLine($"| Email: {this.Email1}");
+        }
+    }
+}
---
+++ C#/FPT EDU/Console/10. Regex and Try Catch/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using _10.Regex_and_Try_Catch.Models;
+
+namespace _10.Regex_and_Try_Catch
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student st = new Student();
+            Console.WriteLine("| Input Student");
+
+            //roll
+            Console.Write("| Roll: ");
+            string roll = Console.ReadLine();
+            Regex r = new Regex(@"^(HE|SE)\d{6}$");
+            if (r.IsMatch(roll))
+            {
+                Console.WriteLine("| Roll Valid");
+                st.Roll1 = roll;
+            }
+            else
+            {
+                Console.WriteLine("| Invalid Roll");
+                goto End;
+            }
+            Console.Write("| Email: ");
+            string email = Console.ReadLine();
+            Regex nFPTMail = new Regex(@"^\w+@\w+.\w+(.vn)");
+            Regex nGmail = new Regex(@"^\w+@\w+.\w+$");
+            if (nFPTMail.IsMatch(email) || nGmail.IsMatch(email))
+            {
+                Console.WriteLine("| Email Valid");
+                st.Email1 = email;
+            }
+            else
+            {
+                Console.WriteLine("| Invalid Email");
+                goto End;
+            }
+
+            Console.Write($"| Name: ");
+            string name = Console.ReadLine();
+            Regex n = new Regex(@"[{(*+?^$|@]|[0-9]");
+            if (!n.IsMatch(name))
+            {   
+                Console.WriteLine("| Name Valid");
+                st.Name1 = name;
+            }
+            else
+            {
+                Console.WriteLine("| Invalid Name");
+                goto End;
+            }
+
+
+            Console.WriteLine("\n| Show Student: ");
+            Console.WriteLine($"| Roll: {st.Roll1.ToString()}");
+            Console.WriteLine($"| Email: {st.Email1.ToString()}");
+            Console.WriteLine($"| Name: {st.Name1.ToString()}");
+
+            End:
+            Console.ReadLine();
+        }
+    }
+}
