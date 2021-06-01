f8d45629e50436ecb616de9063086f75e585daa3
Lục Dương Hải <haild.works@gmail.com>
2021-06-01 17:19:48 +0700

    Add files via upload

---
+++ C#/FPT EDU/LAB C#/Validate/Labs2/SalaryManagerment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary;
+using SalaryManagerment.Controllers;
+
+namespace SalaryManagerment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Human Resource Management ----");
+            ManagerController mc = new ManagerController();
+            mc.AddStaff();
+            mc.DisplayStaff();
+            Console.WriteLine("\n-----------------------------------");
+            mc.AddManager();
+            mc.DisplayManager();
+            Console.ReadLine();
+        }
+    }
+}
