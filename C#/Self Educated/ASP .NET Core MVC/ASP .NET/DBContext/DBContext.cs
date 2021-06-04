c626ddb24bae408cc095bc0bf30aabb5bd2e16fa
Lục Dương Hải <haild.works@gmail.com>
2021-06-04 18:44:07 +0700

    DBContext Example Using DAL Database DLL

---
+++ C#/Self Educated/ASP .NET Core MVC/ASP .NET/DBContext/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL_Database;
+using Microsoft.Extensions.Configuration;
+
+namespace _6._Generic_Reponsitory.Models.DBContext
+{
+    public class DBContext
+    {
+        public string GetConnection()
+        {
+            DBConnection dbCon = new DBConnection();
+            var conStr = dbCon.GetConnectionString();
+            return conStr;
+        }
+    }
+}
---
+++ C#/Self Educated/ASP .NET Core MVC/ASP .NET/DBContext/DirectorDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using _6._Generic_Reponsitory.Models.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace _6._Generic_Reponsitory.Models.LoadDB
+{
+    public class DirectorDB
+    {
+        public SqlCommand cmd = new SqlCommand();
+        private DataSet ds = new DataSet();
+        private SqlDataAdapter da = new SqlDataAdapter();
+        private string status = String.Empty;
+        private static DBContext.DBContext db = new DBContext.DBContext();
+        private SqlDataReader dr;
+        private SqlConnection con = new SqlConnection();
+
+
+        public List<Director> GetDirectors()
+        {
+            List<Director> list = new List<Director>();
+            try
+            {
+                con.ConnectionString = db.GetConnection();
+                con.Open();
+                cmd.Connection = con;
+                string sql = "SELECT * FROM DIRECTOR";
+                cmd.CommandText = sql;
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(new Director()
+                    {
+                        DID = dr.GetInt32(0),
+                        DName = dr.GetString(1),
+                        DNation = dr.GetString(2)
+                    });
+                }
+                cmd.Clone();
+            }
+            catch (Exception e)
+            {
+                status = "Error while at GetDirector Func " + e.Message;
+                throw;
+            }
+            return list;
+        }
+    }
+}
