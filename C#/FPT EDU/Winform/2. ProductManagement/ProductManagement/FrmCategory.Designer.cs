21122b3c0d9e62f8f8990140c12958b08942861b
Lục Dương Hải <haild.works@gmail.com>
2021-06-09 00:55:43 +0700

    Update PRJ02

---
+++ C#/FPT EDU/Winform/2. ProductManagement/ProductManagement/FrmCategory.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ProductManagement
+{
+    partial class FrmCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.dgvCategory = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCatName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCatId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý Danh mục sản phẩm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên danh mục";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(112, 65);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(199, 23);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // dgvCategory
+            // 
+            this.dgvCategory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategory.Location = new System.Drawing.Point(6, 97);
+            this.dgvCategory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvCategory.Name = "dgvCategory";
+            this.dgvCategory.RowHeadersVisible = false;
+            this.dgvCategory.RowHeadersWidth = 82;
+            this.dgvCategory.RowTemplate.Height = 33;
+            this.dgvCategory.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCategory.Size = new System.Drawing.Size(424, 280);
+            this.dgvCategory.TabIndex = 3;
+            this.dgvCategory.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategory_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.btnAddNew);
+            this.groupBox1.Controls.Add(this.txtDescription);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtCatName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCatId);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(448, 98);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(310, 278);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chi tiết";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(220, 243);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(76, 27);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(123, 243);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(67, 27);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNew.Location = new System.Drawing.Point(16, 243);
+            this.btnAddNew.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(76, 27);
+            this.btnAddNew.TabIndex = 10;
+            this.btnAddNew.Text = "Thêm mới";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(123, 94);
+            this.txtDescription.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(174, 137);
+            this.txtDescription.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mô tả chi tiết";
+            // 
+            // txtCatName
+            // 
+            this.txtCatName.Location = new System.Drawing.Point(123, 60);
+            this.txtCatName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCatName.Name = "txtCatName";
+            this.txtCatName.Size = new System.Drawing.Size(174, 23);
+            this.txtCatName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 64);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tên danh mục";
+            // 
+            // txtCatId
+            // 
+            this.txtCatId.Location = new System.Drawing.Point(123, 26);
+            this.txtCatId.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCatId.Name = "txtCatId";
+            this.txtCatId.Size = new System.Drawing.Size(174, 23);
+            this.txtCatId.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 30);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Mã danh mục";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(322, 63);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(108, 25);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // FrmCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(685, 383);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvCategory);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "FrmCategory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Danh mục sản phẩm";
+            this.Load += new System.EventHandler(this.FrmCategory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategory)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.DataGridView dgvCategory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCatName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCatId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ C#/FPT EDU/Winform/2. ProductManagement/ProductManagement/Models/Categories.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using ProductManagement.DAO;
+
+namespace ProductManagement.Models
+{
+    public class Categories
+    {
+        private string cateID;
+        private string cateName;
+        private string cateDescription;
+        public Categories()
+        {
+        }
+
+        public Categories(string cateId, string cateName, string cateDescription)
+        {
+            cateID = cateId;
+            this.cateName = cateName;
+            this.cateDescription = cateDescription;
+        }
+
+        public string CateID { get => cateID; set => cateID = value; }
+        public string CateName { get => cateName; set => cateName = value; }
+        public string CateDescription { get => cateDescription; set => cateDescription = value; }
+
+
+        public static List<Categories> GetCategories()
+        {
+            DataTable cateTable = Database.GetDataBySQL("SELECT *  FROM dbo.Categories");
+            List<Categories> list = new List<Categories>();
+
+            foreach (DataRow item in cateTable.Rows)
+            {
+                Categories cate = new Categories();
+                cate.CateID = item["CategoryID"].ToString();
+                cate.CateName = item["CategoryName"].ToString();
+                cate.CateDescription = item["Description"].ToString();
+                list.Add(cate);
+            }
+            return list;
+        }
+
+        public static DataTable getCategoryByID(string id)
+        {
+            string sql = "SELECT *  FROM dbo.Categories WHERE CategoryId = '" + id + "'";
+            DataTable cateTable = Database.GetDataBySQL(sql);
+            return cateTable;
+        }
+        public static DataTable getCategoryByName(string Name)
+        {
+            string sql = "SELECT *  FROM dbo.Categories WHERE CategoryName = '" + Name + "'";
+            DataTable cateTable = Database.GetDataBySQL(sql);
+            return cateTable;
+        }
+
+        public static int AddCategory(ArrayList arrayList)
+        {
+            string sql = "INSERT INTO dbo.Categories( CategoryId ,CategoryName ,Description) VALUES ( @CategoryId ,  @CategoryName , @Description)";
+
+            Categories cate = new Categories();
+            foreach (Categories item in arrayList)
+            {
+                cate.CateID = item.CateID;
+                cate.CateName = item.CateName;
+                cate.cateDescription = item.CateDescription;
+            }
+
+            SqlParameter[] sqlParameters = new SqlParameter[3];
+            sqlParameters[0] = new SqlParameter("@CategoryId", SqlDbType.Char);
+            sqlParameters[0].Value = cate.CateID;
+            sqlParameters[1] = new SqlParameter("@CategoryName", SqlDbType.NVarChar);
+            sqlParameters[1].Value = cate.CateName;
+            sqlParameters[2] = new SqlParameter("@Description", SqlDbType.NText);
+            sqlParameters[2].Value = cate.CateDescription;
+
+            int rs = Database.ExecuteSQL(sql, sqlParameters);
+            return rs;
+        }
+        public static int UpdateCategory(ArrayList arrayList)
+        {
+            string sql = "UPDATE dbo.Categories SET CategoryName = @CategoryName, [Description] = @Description WHERE CategoryId = @CategoryId";
+
+            Categories cate = new Categories();
+            foreach (Categories item in arrayList)
+            {
+                cate.CateID = item.CateID;
+                cate.CateName = item.CateName;
+                cate.cateDescription = item.CateDescription;
+            }
+
+            SqlParameter[] sqlParameters = new SqlParameter[3];
+            sqlParameters[0] = new SqlParameter("@CategoryId", SqlDbType.Char);
+            sqlParameters[0].Value = cate.CateID;
+            sqlParameters[1] = new SqlParameter("@CategoryName", SqlDbType.NVarChar);
+            sqlParameters[1].Value = cate.CateName;
+            sqlParameters[2] = new SqlParameter("@Description", SqlDbType.NText);
+            sqlParameters[2].Value = cate.CateDescription;
+
+            int rs = Database.ExecuteSQL(sql, sqlParameters);
+            return rs;
+        }
+
+        public static int DeleteCategory(string id)
+        {
+            string sql = "DELETE dbo.Categories WHERE CategoryId = @CategoryId";
+            Categories cate = new Categories();
+            cate.CateID = id;
+
+            SqlParameter[] sqlParameters = new SqlParameter[1];
+            sqlParameters[0] = new SqlParameter("@CategoryId", SqlDbType.Char);
+            sqlParameters[0].Value = cate.CateID;
+            
+            int rs = Database.ExecuteSQL(sql, sqlParameters);
+            return rs;
+        }
+    }
+}
