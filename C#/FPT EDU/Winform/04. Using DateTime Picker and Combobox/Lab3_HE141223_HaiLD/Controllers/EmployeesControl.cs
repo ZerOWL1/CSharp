bc28290592a85fd8176abc796d244f72aa804674
Lục Dương Hải <haild.works@gmail.com>
2021-06-16 00:17:36 +0700

    Update 04. PRJ

---
+++ C#/FPT EDU/Winform/04. Using DateTime Picker and Combobox/Lab3_HE141223_HaiLD/Controllers/EmployeesControl.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Lab3_HE141223_HaiLD.Models.DBContext;
+using Lab3_HE141223_HaiLD.Models.Entity;
+
+namespace Lab3_HE141223_HaiLD.Controllers
+{
+    public class EmployeesControl
+    {
+        private Employees em = new Employees();
+
+        //Get all Employees Method return ArrayList
+        public static ArrayList GetEmployees()
+        {
+            ArrayList list = new ArrayList();
+            string sql = "SELECT * FROM dbo.Employees";
+            DataTable employDt = Database.GetDataBySQL(sql);
+            foreach (DataRow item in employDt.Rows)
+            {
+                Employees cate = new Employees();
+                cate.EID = int.Parse(item["EmployeeID"].ToString());
+                cate.FirstName = item["FirstName"].ToString();
+                cate.BirthDate = DateTime.Parse(item["BirthDate"].ToString()).Date;
+                cate.ReportTo = int.Parse(item["ReportsTo"].ToString());
+                list.Add(cate);
+            }
+            return list;
+        }
+
+        //Get all Employees Method return DataTable
+        public static DataTable GetEmployeesDT()
+        {
+            string sql = "SELECT * FROM dbo.Employees";
+            DataTable employDt = Database.GetDataBySQL(sql);
+            return employDt;
+        }
+
+        //Get manager by ReportTo ID
+        public static DataTable GetManagerByReportTo(string id)
+        {
+            string sql = "SELECT * FROM dbo.Employees WHERE ReportsTo = '"+id+"'";
+            DataTable employDt = Database.GetDataBySQL(sql);
+            return employDt;
+        }
+
+        //Get OrderByID to check Delete func
+        public static DataTable GetOrderByID(string id)
+        {
+            return Database.GetDataBySQL("SELECT * FROM dbo.Orders WHERE EmployeeID = '" + id + "'");
+        }
+
+        //Get TerritoryByID to check Delete func
+        public static DataTable GetTerritoryByID(string id)
+        {
+            return Database.GetDataBySQL("SELECT * FROM dbo.EmployeeTerritories WHERE EmployeeID = '"+id+"'");
+        }
+
+        //Delete Func
+        public static int DeleteEmployees(string id)
+        {
+            //Declare a int result
+            int result = 0;
+            //Check if table Order or Territories data exist or not
+            if (GetOrderByID(id).Rows.Count > 0 || GetTerritoryByID(id).Rows.Count > 0)
+            {
+                //Exist return 0 and show message
+                result = 0;
+            }
+            //Check if data Manager exist or not
+            else if(GetManagerByReportTo(id).Rows.Count > 0)
+            {
+                //Exist return -1 and show message
+                result = -1;
+            }
+            else
+            {
+                //Delete Func
+                string sql = "DELETE FROM dbo.Employees WHERE EmployeeID = @eid";
+                SqlParameter parameter = new SqlParameter("@eid", SqlDbType.Int);
+                parameter.Value = id;
+                result = Database.ExecuteSQL(sql, parameter);
+            }
+            return result;
+        }
+
+        //Add Method
+        public static int AddEmployee(ArrayList list)
+        {
+            string sql = "INSERT INTO Employees(FirstName, BirthDate, ReportsTo) VALUES ( @FirstName ,  @BirthDate, @ReportsTo)";
+
+            SqlParameter[] sqlParameters = new SqlParameter[]
+            {
+                new SqlParameter("@FirstName ", SqlDbType.NVarChar),
+                new SqlParameter("@BirthDate", SqlDbType.DateTime),
+                new SqlParameter("@ReportsTo", SqlDbType.Int),
+            };
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sqlParameters[i].Value = list[i];
+            }
+
+            return Database.ExecuteSQL(sql, sqlParameters);
+        }
+    }
+}
---
+++ C#/FPT EDU/Winform/04. Using DateTime Picker and Combobox/Lab3_HE141223_HaiLD/Models/DBContext/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+namespace Lab3_HE141223_HaiLD.Models.DBContext
+{
+    public class Database
+    {
+
+        private static SqlCommand cmd;
+        private static SqlDataAdapter da = new SqlDataAdapter();
+
+        public static SqlConnection GetConnection()
+        {
+            string strCon = ConfigurationManager.ConnectionStrings["LAB3"].ToString();
+            return new SqlConnection(strCon);
+        }
+
+        //SELECT Method
+        public static DataTable GetDataBySQL(string sql)
+        {
+            DataSet ds = new DataSet();
+            DataTable table = new DataTable();
+            cmd = new SqlCommand(sql, GetConnection());
+            da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        //Insert, Update, Delete have to open Connected
+        public static int ExecuteSQL(string sql, params SqlParameter[] sqlParameter)
+        {
+            cmd = new SqlCommand(sql, GetConnection());
+            //Add using Parameter
+            cmd.Parameters.AddRange(sqlParameter);
+            cmd.Connection.Open();
+            int rs = cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+            return rs;
+        }
+    }
+}
